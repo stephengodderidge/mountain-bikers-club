c66ea60b738566a60349937e095fd81fdc9052ed
Cédric Eberhardt <design@cedeber.fr>
2018-09-02 16:10:52 +0200

    better form field + error message

--- member/models.py
+++ member/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 from trail.models import Trail
 
 
 class User(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        blank=False,
+        error_messages={
+            'unique': _("A user with that email address already exists."),
+        },
+    )
     favorite_trails = models.ManyToManyField(Trail, related_name='favorite_by')
-
-    objects = UserManager()
-
