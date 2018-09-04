ee26b64bed1341af5ca7c51f607c0127ad03aaad
Cédric Eberhardt <design@cedeber.fr>
2018-09-04 18:31:25 +0200

    add autofocus attr to trail edit form fix #20

--- trail/forms.py
+++ trail/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput
 
 from .models import Trail
 
@@ -19,6 +20,9 @@ class GpxEditForm(forms.ModelForm):
     class Meta:
         model = Trail
         fields = ('name', 'description')
+        widgets = {
+            'name': TextInput(attrs={'autofocus': True})
+        }
 
     def save(self, commit=True):
         trail = super().save(commit=False)
