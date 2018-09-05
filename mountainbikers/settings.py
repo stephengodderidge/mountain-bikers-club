e2a0e56bae4e1eaed0cf613a2d17f1db46671649
Cédric Eberhardt <design@cedeber.fr>
2018-09-05 18:24:25 +0200

    add s3 for CSP

--- mountainbikers/settings.py
+++ mountainbikers/settings.py
@@ -203,7 +203,7 @@ CSP_SCRIPT_SRC = ("'self'", 'cdnjs.cloudflare.com')
 CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com')
 CSP_FONT_SRC = ("'self'", '')
 CSP_IMG_SRC = ("'self'", '*.tile.opentopomap.org')
-CSP_CONNECT_SRC = ("'self'", 'wss:')
+CSP_CONNECT_SRC = ("'self'", 'wss:', 'mountain-bikers-club.s3.amazonaws.com')
 CSP_FRAME_ANCESTORS = ("'none'",)
 
 
