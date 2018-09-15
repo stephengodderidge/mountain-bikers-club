7182a90db83a3e3c7f3af87f17656b86de0c805f
Cédric Eberhardt <design@cedeber.fr>
2018-09-15 12:37:31 +0200

    csp

--- mountainbikers/settings.py
+++ mountainbikers/settings.py
@@ -206,7 +206,7 @@ CSP_DEFAULT_SRC = ("'none'",)
 CSP_SCRIPT_SRC = ("'self'", 'cdnjs.cloudflare.com', 'api.mapbox.com')
 CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com', 'api.mapbox.com')
 CSP_FONT_SRC = ("'self'", '')
-CSP_IMG_SRC = ("'self'", '*.tile.opentopomap.org', 'mountain-bikers-club.s3.amazonaws.com', 'api.mapbox.com')
+CSP_IMG_SRC = ("'self'", '*.tile.opentopomap.org', 'mountain-bikers-club.s3.amazonaws.com', 'cdnjs.cloudflare.com', 'api.mapbox.com')
 CSP_CONNECT_SRC = ("'self'", 'wss:', 'mountain-bikers-club.s3.amazonaws.com')
 CSP_FRAME_ANCESTORS = ("'none'",)
 
