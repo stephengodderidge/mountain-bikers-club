2f14ae1fdb5f188bf2f240ccada5ab2c2fc566dc
Cédric Eberhardt <design@cedeber.fr>
2018-09-15 09:49:14 +0200

    add fullscreen option to leaflet

--- mountainbikers/settings.py
+++ mountainbikers/settings.py
@@ -203,10 +203,10 @@ if not DEBUG:
 # Content Security Policy
 
 CSP_DEFAULT_SRC = ("'none'",)
-CSP_SCRIPT_SRC = ("'self'", 'cdnjs.cloudflare.com')
-CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com')
+CSP_SCRIPT_SRC = ("'self'", 'cdnjs.cloudflare.com', 'api.mapbox.com')
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com', 'api.mapbox.com')
 CSP_FONT_SRC = ("'self'", '')
-CSP_IMG_SRC = ("'self'", '*.tile.opentopomap.org', 'mountain-bikers-club.s3.amazonaws.com')
+CSP_IMG_SRC = ("'self'", '*.tile.opentopomap.org', 'mountain-bikers-club.s3.amazonaws.com', 'api.mapbox.com')
 CSP_CONNECT_SRC = ("'self'", 'wss:', 'mountain-bikers-club.s3.amazonaws.com')
 CSP_FRAME_ANCESTORS = ("'none'",)
 
