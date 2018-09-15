52a17d198d2c47ce410f7a27f490315ebb030ad9
Cédric Eberhardt <design@cedeber.fr>
2018-09-15 12:20:32 +0200

    draw polyline from mbc api

--- shell/static/shell/scripts/components/trail-map.js
+++ shell/static/shell/scripts/components/trail-map.js
@@ -3,8 +3,9 @@ import { loadJS, loadCSS } from '../library/async-loader.js';
 export default class Map extends HTMLElement {
     constructor() {
         super();
-        this.url = this.dataset.url;
         this.hasInterface = Boolean(this.dataset.hasInterface);
+        this.trail_id = this.dataset.trail;
+        this.track_id = this.dataset.track;
     }
 
     async connectedCallback() {
@@ -14,8 +15,9 @@ export default class Map extends HTMLElement {
         await loadJS('https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.js', 'anonymous');
         await loadJS('https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js', 'anonymous');
         await loadJS('https://cdnjs.cloudflare.com/ajax/libs/leaflet-minimap/3.6.1/Control.MiniMap.min.js', 'anonymous');
-        // TODO build our own loader from DB
-        await loadJS('https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.4.0/gpx.min.js', 'anonymous');
+
+        const data = await fetch(`/trail/api/${this.trail_id}/track/${this.track_id}`)
+            .then(response => response.json());
 
         const mapOptions = {
             scrollWheelZoom: false,
@@ -43,26 +45,37 @@ export default class Map extends HTMLElement {
         }
         const myMap = window.L.map(this, mapOptions);
 
-        const tileLayer = new window.L.tileLayer('https://b.tile.opentopomap.org/{z}/{x}/{y}.png', {
-            attribution:
-                '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, &copy; <a href="https://opentopomap.org">OpenTopoMap</a>',
+        const tileLayer = new window.L.tileLayer('https://a.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            attribution: false,
             maxZoom: 17,
         });
 
-        new window.L.GPX(this.url, {
-            async: true,
-            polyline_options: {
-                color: 'crimson',
+        new window.L.tileLayer('https://b.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, &copy; <a href="https://opentopomap.org">OpenTopoMap</a>',
+            maxZoom: 17,
+        }).addTo(myMap);
+
+        if (data.points && data.points.length > 3) {
+            const points = data.points.map(p => [p.latitude, p.longitude]);
+            const polylineOptionsBack = {
+                color: 'white',
                 opacity: 0.75,
-                weight: 4,
+                weight: 11,
                 lineCap: 'round',
-            },
-            marker_options: markerOptions,
-        })
-            .on('loaded', function(e) {
-                myMap.fitBounds(e.target.getBounds());
-                new window.L.Control.MiniMap(tileLayer).addTo(myMap);
-            })
-            .addTo(myMap);
+            };
+            const polylineOptionsFront = {
+                color: '#2E73B8',
+                weight: 5,
+                lineCap: 'round',
+            };
+            const polylineBack = window.L.polyline(points, polylineOptionsBack).addTo(myMap);
+            window.L.polyline(points, polylineOptionsFront).addTo(myMap);
+
+            myMap.fitBounds(polylineBack.getBounds());
+            new window.L.Control.MiniMap(tileLayer, {
+                zoomLevelFixed: 5,
+            }).addTo(myMap);
+        }
     }
 }
