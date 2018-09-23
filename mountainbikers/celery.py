1ca7183fef68b8fc51fa345247fc48ca82464641
Cédric Eberhardt <design@cedeber.fr>
2018-09-23 09:53:38 +0200

    remove debug task

--- mountainbikers/celery.py
+++ mountainbikers/celery.py
@@ -6,8 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mountainbikers.settings')
 app = Celery('mountainbikers')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
