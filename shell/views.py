56a036f1f84e5893d0bd0e9549fed69553e32f8f
Cédric Eberhardt <design@cedeber.fr>
2018-09-24 21:39:50 +0200

    rename files

--- shell/views.py
+++ shell/views.py
@@ -7,11 +7,11 @@ def bokeh_js(request):
     context = {
         'files': INLINE.js_raw,
     }
-    return render(request, 'shell/content.html', context, content_type='application/javascript')
+    return render(request, 'shell/vendor_files_content.html', context, content_type='application/javascript')
 
 
 def bokeh_css(request):
     context = {
         'files': INLINE.css_raw,
     }
-    return render(request, 'shell/content.html', context, content_type='text/css')
+    return render(request, 'shell/vendor_files_content.html', context, content_type='text/css')
