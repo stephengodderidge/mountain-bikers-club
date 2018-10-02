65b5e5f7894e0b4e12fbe7ef0d0dff41b22d0202
Cédric Eberhardt <design@cedeber.fr>
2018-10-02 09:06:09 +0200

    new string way of formatting

--- trail/templatetags/filter_gpx.py
+++ trail/templatetags/filter_gpx.py
@@ -16,7 +16,7 @@ def format_time(time):
     minutes = math.floor(time_s / 60.)
     hours = math.floor(minutes / 60.)
 
-    return '%s:%s:%s' % (str(int(hours)).zfill(2), str(int(minutes % 60)).zfill(2), str(int(time_s % 60)).zfill(2))
+    return '{}:{}:{}'.format(str(int(hours)).zfill(2), str(int(minutes % 60)).zfill(2), str(int(time_s % 60)).zfill(2))
 
 
 @register.filter
